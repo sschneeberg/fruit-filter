a988a06916fda1bf185fa9fefddf63f9421a64d0
sschneeberg <simone.m.schneeberg@gmail.com>
2020-12-10 11:41:57 -0500

    Create class components

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react';
 
-class Input extends Component {}
+class Input extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    render() {
+        return <div className="Input">Input</div>;
+    }
+}
 
 export default Input;
--- src/Components/List.js
+++ src/Components/List.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react';
 
-class List extends Component {}
+class List extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    render() {
+        return <div className="List">List</div>;
+    }
+}
 
 export default List;
